d0ee4f32aed3cecd43fdccdb7ea09bba3ae621ee
raorao1011 <raorao1011@gmail.com>
2023-11-26 17:16:29 +0900

    cssCodeSplit

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig(async () => {
   return {
     build: {
       assetsInlineLimit: 20480,
-      cssCodeSplit: false,
+      cssCodeSplit: true,
       cssTarget: 'es6',
       minify: true,
       rollupOptions: {
